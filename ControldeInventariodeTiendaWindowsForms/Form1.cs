296010857ce805143d72a8b0a35ebaad771954c0
seb <snapsync29@gmail.com>
2026-02-06 18:30:22 -0600

    Agregar archivos de proyecto.

---
+++ ControldeInventariodeTiendaWindowsForms/Form1.cs
@@ -0,0 +1,10 @@
+namespace ControldeInventariodeTiendaWindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ControldeInventariodeTiendaWindowsForms/Producto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControldeInventariodeTiendaWindowsForms
+{
+    internal class Producto
+    {
+        protected int id;
+        protected string nombre;
+        protected decimal precio;
+        protected int stockactual;
+        protected int stockminimo;
+
+
+
+
+    }
+}
